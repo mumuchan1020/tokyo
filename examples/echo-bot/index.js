809480627f76f87169c00d884332086b0a8be9b6
mumuchan1020 <165268009+mumuchan1020@users.noreply.github.com>
2024-05-20 13:31:24 +0800

    index.js

--- examples/echo-bot/index.js
+++ examples/echo-bot/index.js
@@ -1,24 +1,25 @@
 'use strict';
 
-const line = require('@line/bot-sdk');
 const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+const line = require('@line/bot-sdk');
 
-// create LINE SDK config from env variables
+// 從環境變量中創建LINE SDK配置
 const config = {
   channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
 };
 
-// create LINE SDK client
-const client = new line.messagingApi.MessagingApiClient({
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
-});
+// 創建LINE SDK客戶端
+const client = new line.Client(config);
 
-// create Express app
-// about Express itself: https://expressjs.com/
+// 創建Express應用
+// 關於Express本身的更多信息: https://expressjs.com/
 const app = express();
 
-// register a webhook handler with middleware
-// about the middleware, please refer to doc
+// 使用中介軟體註冊Webhook處理程序
+// 關於中介軟體的更多信息，請參閱文檔
 app.post('/callback', line.middleware(config), (req, res) => {
   Promise
     .all(req.body.events.map(handleEvent))
@@ -29,21 +30,52 @@ app.post('/callback', line.middleware(config), (req, res) => {
     });
 });
 
-// event handler
-function handleEvent(event) {
+// 事件處理函數
+async function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
-    // ignore non-text-message event
+    // 忽略非簡訊事件
     return Promise.resolve(null);
   }
 
-  // create an echoing text message
-  const echo = { type: 'text', text: event.message.text };
+  // 將消息內容轉換為小寫
+  const message = event.message.text.toLowerCase();
+  let commandUrl = '';
 
-  // use reply API
-  return client.replyMessage({
-    replyToken: event.replyToken,
-    messages: [echo],
-  });
+  // 根據消息內容構建Arduino控制URL
+  if (message.includes('turn on 16')) {
+    commandUrl = 'http://<arduino-ip-address>/16/on';
+  } else if (message.includes('turn off 16')) {
+    commandUrl = 'http://<arduino-ip-address>/16/off';
+  } else if (message.includes('turn on 17')) {
+    commandUrl = 'http://<arduino-ip-address>/17/on';
+  } else if (message.includes('turn off 17')) {
+    commandUrl = 'http://<arduino-ip-address>/17/off';
+  }
+
+  // 如果commandUrl不為空，發送請求到Arduino
+  if (commandUrl) {
+    try {
+      await axios.get(commandUrl);
+      // 回復用戶，命令已發送到Arduino
+      return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: `命令已發送到Arduino: ${message}`
+      });
+    } catch (error) {
+      console.error('發送命令到Arduino時出錯:', error);
+      // 回復用戶，發送命令失敗
+      return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: `發送命令到Arduino失敗: ${message}`
+      });
+    }
+  } else {
+    // 回復用戶，無效的命令
+    return client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: `無效的命令: ${message}`
+    });
+  }
 }
 
 // listen on port
