62a730171e50910b2473b957a9b9911cdd1a77b8
mumuchan1020 <165268009+mumuchan1020@users.noreply.github.com>
2024-05-28 13:40:39 +0800

    Update index.js

--- examples/echo-bot/index.js
+++ examples/echo-bot/index.js
@@ -47,6 +47,7 @@ app.post('/webhook', (req, res) => {
         let commandUrl = '';
         if (receivedMessage === '1' || receivedMessage === 'a') {
             commandUrl = `http://192.168.11.5/16/on`; // 控制設備打開
+          console.log('aa command sent success ---');
         } else if (receivedMessage === '0' || receivedMessage === 'b') {
             commandUrl = `http://192.168.11.5/16/off`; // 控制設備關閉
         } else if (receivedMessage === '2' || receivedMessage === 'c') {
